a43c3aa78916604cbd6327997cb7b10265adc4bf
Alexander Tyurin <darksid1420@gmail.com>
2015-09-16 21:44:10 +0300

    fix noexcept for windows

--- common/exception.hpp
+++ common/exception.hpp
@@ -9,7 +9,11 @@ class exception : public std::exception
 {
 public:
   exception(const std::string &message) : msg(message) { }
+#ifdef __linux__
   virtual char const *what() const noexcept { return msg.c_str(); }
+#elif _WIN32
+  virtual char const *what() const { return msg.c_str(); }
+#endif
 private:
   std::string msg;
 };
